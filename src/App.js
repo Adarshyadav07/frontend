62f2a428856abd80afc981571b99ebd364d78a63
Adarsh <aadarshyadav306@gmail.com>
2025-07-12 20:21:31 +0530

    asa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import './App.css';
-import Headers from "./component/layout/Header.js";
-import { BrowserRouter as Router } from 'react-router-dom';
+import Headers from "./component/layout/Header/Header.js";
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import WebFont from "webfontloader";
 import React from 'react';
-
-
+import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js";
+import ProductDetails from './component/Product/ProductDetails.js';
 
 function App() {
   React.useEffect(() => {
-
-  WebFont.load({
+   WebFont.load({
     google:{
       families:["Roboto", "Droid Sans", "Chilanka"]
-  }
-})
-}, [])
+    }
+   })
+  }, [])
 
 
   return (
     <Router>
-       <Headers />  
+       <Headers /> 
+       <Route extact path='/' Component={Home}/>
+       <Route extact path="/product/:id" Component={ProductDetails}/>
+       
+
+       <Footer />
     </Router>
   );
 }
